f573e3ee647791b102f1fd8e7e3fa8dc5b79f4a7
Thatphum <s6201012620112@email.kmutnb.ac.th>
2020-08-16 21:22:53 +0700

    How to Type casting

---
+++ Type_Casting.java
@@ -0,0 +1,13 @@
+public class Typecasting{
+	public static void main(String[] args){
+		int myInt = 9;
+		double myDouble = myInt;      //Automatic casting : int -> double
+		System.out.println(myDouble); //output : 9.0
+		System.out.println(myInt);    //output : 9
+		
+		double myDoubleTwo = 9.85; 
+		int myNewInt = (int) myDoubleTwo;  //Manual casting : double -> int
+		System.out.println(myDoubleTwo);   //output : 9.85
+		System.out.println(myInt); 	   //output : 9 
+	}
+}
