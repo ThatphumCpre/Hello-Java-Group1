0402affd51699234350b1cf9192a8535c53dc8c5
Thatphum <s6201012620112@email.kmutnb.ac.th>
2020-08-16 18:59:11 +0700

    Variable Type

---
+++ Variable.java
@@ -0,0 +1,14 @@
+public class HelloJava { 
+	public static void main(String[] args) {
+		String name = "John";  //String collect as message or word 
+		System.out.println(name);
+		int myNum = 15;		//int collect as Intreger 
+		System.out.println(myNum);
+		char myChar = 'F'; 	//char collect as single or character
+		System.out.println(myChar); 
+		float myFloat = 5.99F;  //float collect as decimal if don't f means collect with double  
+		System.out.println(myFloat);
+		boolean myBoolean = true; //collect as boolean 
+		System.out.println(myBoolean);  
+	}
+}
