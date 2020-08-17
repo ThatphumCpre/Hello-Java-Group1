7afd4e97bffb1c11d7c25820fa76d3e1b5a9701a
Thatphum <s6201012620112@email.kmutnb.ac.th>
2020-08-17 20:23:01 +0700

    Basic Method

---
+++ Method.java
@@ -0,0 +1,8 @@
+public class Method {
+    public static void main(String[] args) {
+      myMethod(); //use "myMethod" method
+    }
+    static void myMethod(){   //create static method name  myMethod
+        System.out.println("Hi i'm from myMethod method");
+    }
+}
