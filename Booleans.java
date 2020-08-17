bd43de1185045cc80f8985572e03f8d2589ab5da
Thannathorn Somton <s6201012610010@email.kmutnb.ac.th>
2020-08-18 04:53:10 +0700

    Booleans

---
+++ Booleans.java
@@ -0,0 +1,21 @@
+public class Booleans {
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 20;
+		Boolean Bool1 = true;
+		Boolean Bool2 = false;
+
+		System.out.println(Bool1);
+		// print Boolean value
+		System.out.println(Bool2);
+		// print Boolean value
+		System.out.println(a < b);
+		// return true because 20 is greater than 10
+		System.out.println(a > b);
+		// return false because 10 is less than 20 
+
+
+
+	}
+}
